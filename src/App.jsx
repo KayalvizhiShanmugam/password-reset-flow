7bf916d84e1859862b38300049671f7051112b18
KayalvizhiShanmugam <s.ksyalvizhi00@gmail.com>
2023-05-05 13:09:07 +1400

    password reset

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter,Switch,Route,Redirect } from 'react-router-dom';
+
+import Login from './pages/login';
+import ForgotPassWord from './pages/forgotPassword';
+import Register from './pages/register';
+import Protected from './pages/protected';
+import ChangePassword from './pages/changePassword';
+
+
+export const AppContext=React.createContext()
+
+function App() {
+const [log,setLog]= React.useState(false)
+  return (
+    <div className="App">
+        <BrowserRouter>
+        <Switch>
+          <AppContext.Provider value={[log,setLog]}>
+          <Route path="/protected" component={Protected} />
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/forgot" component={ForgotPassWord}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/resetPassword/:id/:token" component={ChangePassword}/>
+          <Route exact path="/" >
+            <Redirect to="/login"/>
+          </Route>
+          </AppContext.Provider>
+        </Switch>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
